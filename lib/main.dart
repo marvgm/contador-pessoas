242dca29d749097b2026dd90cb27cc45738e00df
marcosgracioli <marvgm@gmail.com>
2020-05-02 12:05:02 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String infoText = "Pode entrar!";
+
+  void _changePeople(int value){
+    setState(() {
+      _people += value;
+
+      if (_people <0){
+        infoText = "Inpossível!";
+      }else if(_people <=10) {
+        infoText = "Pode entrar!";
+      } else{
+        infoText = "Capacidade Total atingida!";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/original.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        _changePeople(1);
+                      },
+                      child: Text("+1",
+                          style:
+                              TextStyle(fontSize: 40.0, color: Colors.white))),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      onPressed: () {
+                        _changePeople(-1);
+                      },
+                      child: Text("-1",
+                          style:
+                              TextStyle(fontSize: 40.0, color: Colors.white))),
+                ),
+              ],
+            ),
+            Text(
+              infoText,
+              style: TextStyle(
+                  color: Colors.green,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
